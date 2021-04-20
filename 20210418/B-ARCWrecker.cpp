ab0cd40b713483f0e490973e70f606c66ebe26ac
隼田駿大 <koda123456789koda@yahoo.co.jp>
2021-04-21 00:12:55 +0900

    add B

---
+++ 20210418/B-ARCWrecker.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+    int N;
+    std::vector<int> vec;
+    std::cin >> N;
+    for(int i=0;i<N;i++){
+        int Ai;
+        std::cin >> Ai;
+        vec.push_back(Ai);
+    }
+
+    
+}
