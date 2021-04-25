6c2ad858473bb978eb957a7b190e48e4732bcb43
隼田駿大 <koda123456789koda@yahoo.co.jp>
2021-04-25 12:21:09 +0900

    B

--- 20210418/B-ARCWrecker.cpp
+++ 20210418/B-ARCWrecker.cpp
@@ -4,13 +4,44 @@
 int main(void)
 {
     int N;
-    std::vector<int> vec;
+    int mod = 1000000007;
+    int answer = 1;
+    std::vector<int> A,Q;
     std::cin >> N;
-    for(int i=0;i<N;i++){
+    for(int i=1;i<=N;i++){
         int Ai;
         std::cin >> Ai;
-        vec.push_back(Ai);
+        A.push_back(Ai);
+    }
+    std::sort(A.begin(), A.end());
+
+    Q.push_back(A[0]);
+    for(int i=1;i<N;i++){
+        Q.push_back(A[i]-A[i-1]);
+    }
+    
+    for(int i=0;i<N;i++){
+        answer *= (Q[i] + 1);
+        answer %= mod;
     }
 
-    
+
+
+
+    /* std::cout << "A:"; */
+    /* for(int i=0;i<N;i++){ */
+    /*     std::cout << A[i] << " "; */
+    /* } */
+    /* std::cout << std::endl; */
+
+    /* std::cout << "Q:"; */
+    /* for(int i=0;i<N;i++){ */
+    /*     std::cout << Q[i] << " "; */
+    /* } */
+    /* std::cout << std::endl; */
+
+
+    std::cout << answer << std::endl;
+    return 0;
+
 }
